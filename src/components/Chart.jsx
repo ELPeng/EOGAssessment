2f3aee6cf1a916002a7dc092579e5441df5e4a5d
EPeng <erpengic@gmail.com>
2021-11-08 14:33:09 -0600

    completed multi dataset functionality for the chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,59 +1,83 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-adapter-moment';
+
 import { fetchValuesArr } from '../api';
-// import { Line } from 'react-chartjs-2';
 
 const Chart = ({ metricNames, mData }) => {
-  if (!mData) return 'Loading....';
+  if (!mData || mData.length === 0) return 'Loading....';
   console.log(mData);
   console.log(metricNames);
 
   // Retrieves time array to be set as X-Axis
-  const xVals = mData[0].measurements.map((data) => data.at);
+  const xVals = mData[0].measurements.map((data) => new Date(data.at));
   console.log(xVals);
   const dataArr = fetchValuesArr(mData);
   console.log(dataArr);
 
-  //   const datasets = dataArr.map((set, i) => ({
-  //     label: metricNames[i],
-  //     data: set,
-  //     fill: false,
-  //     borderColor: 'rgb(75, 192, 192)',
-  //     tension: 0.5,
-  //   }));
+  const colorsArr = ['#DC143C', '#FFA500', '#32CD32', '#20B2AA', '#191970', '#9400D3'];
+  const datasets = dataArr.map((set, i) => ({
+    label: metricNames[i],
+    data: set,
+    fill: false,
+    borderColor: colorsArr[i],
+    borderWidth: 1,
+    tension: 0.5,
+  }));
 
-  //   const data = {
-  //     labels: xVals,
-  //     datasets,
-  //   };
+  const data = {
+    labels: xVals,
+    datasets,
+  };
 
-  //   const options = {
-  //     plugins: {
-  //       title: {
-  //         display: true,
-  //         text: 'Chart Title',
-  //       },
-  //     },
-  //     scales: {
-  //       x: {
-  //         type: 'linear',
-  //       },
-  //       y: {
-  //         type: 'linear',
-  //       },
-  //     },
-  //     elements: {
-  //       point: {
-  //         radius: 0,
-  //       },
-  //     },
-  //   };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Historical Data Measurements',
+        font: {
+          size: 24,
+        },
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time of day',
+          font: {
+            size: 16,
+          },
+        },
+        type: 'time',
+        time: {
+          unit: 'hour',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Temperature (F)',
+          font: {
+            size: 14,
+          },
+        },
+        type: 'linear',
+      },
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+  };
 
   return (
     <>
       <div className="header">
         <h1 className="title">Multi Axis Line Chart</h1>
       </div>
-      {/* <Line data={data} options={options} /> */}
+      <Line style={{ padding: '20px' }} data={data} options={options} />
     </>
   );
 };
